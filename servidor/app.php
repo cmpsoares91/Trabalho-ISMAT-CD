73bed4ae3ea20f66f6e4e208df1cfec1467f3efb
Nuno Miguel de Carvalho Ferreira <nuno.carvalhoferreira@outlook.com>
2014-06-24 18:38:15 +0100

    1ºteste

--- servidor/app.php
+++ servidor/app.php
@@ -1,3 +1,24 @@
 <?php
-// put your code here
+if (!$_POST['string'] == ""){
+    $string = $_POST['string'];
+    $min = $_POST['min'];
+    $max = $_POST['max'];
+    $result = $_POST['result'];
+    $size = strlen($result);
+    for ($i=$min; $i<$max; ++$i){
+        $percentagem = ($i/$max)*100;
+        print $percentagem + "% calculado.";
+        $res = hash('sha256', $string+$i);
+        
+        if ($res == $result){
+            ob_start();
+            echo "true";
+            break;
+        }
+    }
+    if ($res =! $result){
+        ob_start();
+        echo "false";
+    }
+}
 ?>
