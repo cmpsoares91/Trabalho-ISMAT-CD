eee1f0f2de1d0fccf7df3b18c1b2949f43d341d2
Carlos Soares <cmpsoares91@gmail.com>
2014-06-25 01:54:17 +0100

    Edited Cliente and slave app to use same json vars

--- Cliente/client.js
+++ Cliente/client.js
@@ -1,5 +1,33 @@
-/*
+/**
  * Applicação de Cliente em Node.js
  */
  var $      = require('jquery');
  var config = require('/config.json');
+ 
+ /**
+ *  An example HTTP POST request.
+ */  
+var postJob = function(URL, blockNum){
+
+	var requestObject = {
+		blockSize: bs || 1000;
+		block: blockNum,
+		baseStr: str || "Hello, World!",
+		objective: objStr || "00000"
+	};
+
+	$.ajax({
+		'url': URL,
+		'type' : 'POST',
+		'headers' : {'Content-Type' : 'application/json'},
+		'data' : JSON.stringify(requestObject), 
+		'processData' : false,
+		'success' : function(data){
+			console.log(data);
+		},
+		'error': function(jqXHR, data){
+			console.log(data);
+	    },
+		'dataType' : 'json'
+	});
+};
--- servidor/app.php
+++ servidor/app.php
@@ -1,24 +1,26 @@
 <?php
-if (!$_POST['string'] == ""){
-    $string = $_POST['string'];
-    $min = $_POST['min'];
-    $max = $_POST['max'];
-    $result = $_POST['result'];
-    $size = strlen($result);
-    for ($i=$min; $i<$max; ++$i){
-        $percentagem = ($i/$max)*100;
-        print $percentagem + "% calculado.";
-        $res = hash('sha256', $string+$i);
-        
-        if ($res == $result){
-            ob_start();
-            echo "true";
-            break;
-        }
-    }
-    if ($res =! $result){
-        ob_start();
-        echo "false";
-    }
-}
+	if (!$_POST['data'] == ""){
+		$data = json_decode($_POST['data']);
+		$string = data->{'baseStr'};
+		$min = data->{'block'} * data->{'blockSize'};
+		$max = $min + data->{'blockSize'} - 1;
+		$result = data->{'objective'};
+		
+		$size = strlen($result);
+		for ($i=$min; $i<$max; ++$i){
+			$percentagem = ($i/$max)*100;
+			print $percentagem + "% calculado.";
+			$res = hash('sha256', $string+$i);
+			
+			if ($res == $result){
+				ob_start();
+				echo "true";
+				break;
+			}
+		}
+		if ($res =! $result){
+			ob_start();
+			echo "false";
+		}
+	}
 ?>
