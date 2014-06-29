cc475acbbf34a0a71502076e62665892dcc7869c
Carlos Soares <cmpsoares91@gmail.com>
2014-06-30 00:45:07 +0100

    for test on server

--- Cliente/client.js
+++ Cliente/client.js
@@ -1,7 +1,8 @@
 /**
  * Applicação de Cliente em Node.js
  */
- var $      = require('jquery');
+ var jsdom  = require('jsdom');
+ var $      = require('jquery')(window);
  var config = require('./config.json'); //config.locations.length e config.locations[0] devem estar a funcionar agora...
  var Q      = require('./Queue.js');
  
