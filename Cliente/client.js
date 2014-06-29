f001fd5952dcd251aeb4109700ecc640cf29fb30
Carlos Soares <cmpsoares91@gmail.com>
2014-06-29 17:36:17 +0100

    Most is correct, getting a TypeError:
    Object function (...) has no method 'ajax'

--- Cliente/client.js
+++ Cliente/client.js
@@ -3,41 +3,79 @@
  */
  var $      = require('jquery');
  var config = require('./config.json'); //config.locations.length e config.locations[0] devem estar a funcionar agora...
- var Q  = require('./Queue.js');
+ var Q      = require('./Queue.js');
+ 
+ //Debug
  console.log(typeof Q.Queue);
+ console.log(config.locations.length);
+ console.log(config.locations[0]);
+ console.log(config.blockSize);
  
  /**
  *  An example HTTP POST request.
  */
 //Global variables
-//var domains = new Array(); //receive domains from config file;
-var postQueue = new Q.Queue(); //Queue with the posts
-var maxQueueSize; //Maximum size of queue will depend of the number of hosts
-var arrayPosts = new Array(); //postJob Object array
+var postQueue    = new Q.Queue(); //Queue with the posts
+var arrayPosts   = new Array(); //postJob Object array
+
+//Load configuration data to variables
+var maxQueueSize = config.locations.length; //Maximum size of queue will depend of the number of hosts
+var bSize, objString, bString;
+//bSize load:
+try {
+	bSize = config.blockSize;
+}
+catch(err) {
+	//Debug:
+	console.log("Couldn't load blockSize from config");
+	console.log(err);
+}
+//objString load:
+try {
+	objString = config.objective;
+}
+catch(err) {
+	//Debug:
+	console.log("Couldn't load objective string from config");
+	console.log(err);
+}
+//bString load:
+try {
+	bString = config.baseString;
+}
+catch(err) {
+	//Debug:
+	console.log("Couldn't load base string from config");
+	console.log(err);
+}
+
+//Run main
+main();
 
 //postJob object
-function postJob(baseString, URL, blockNum){
+function postJob(URL, blockNum){
     
     this.result;
     
-	var requestObject = {};
-	
+	//Define Post request object
+	var requestObject       = {};
 	requestObject.blockSize = bSize || 1000;
-	requestObject.block = blockNum;
-	requestObject.baseStr = baseString || "Hello, World!";
-	requestObject.objective = objStr || "00000";
+	requestObject.base      = bString || "Hello, World!";
+	requestObject.objective = objString || "00000";
+	requestObject.block     = blockNum;
 	
-        var tempresult;
+    var tempresult;
+	
 	$.ajax({
 		'url': URL,
 		'type' : 'POST',
 		'headers' : {'Content-Type' : 'application/json'},
 		'data' : JSON.stringify(requestObject), 
 		'processData' : false,
-		'success' : function(data){
+		'success' : function(data) {
                         tempresult = jQuery.parseJSON(data)
-			console.log(data);
-		},
+						console.log(data);
+					},
 		'error': function(jqXHR, data){
 			console.log(data);
 	    },
@@ -46,34 +84,26 @@ function postJob(baseString, URL, blockNum){
         this.result = tempresult;
 }
 
-//Não é necessário...
-//function to get the domains from config.json
-/*
-function getDomains(){
-    var temp;
-    var tempSize;
-    $.getJSON('config.json', function(response){
-       temp = response;
-       tempSize = Object.keys(response).length;
-    });
-    domains = temp;
-    maxQueueSize = tempsize;
-}
-*/
-
 //main function
 function main(){
-    //local variables
-    var arguments = process.argv.slice(2);//receives arguments from command line
-    var result = false;
+    //Debug
+	console.log("main init");
+	
+	//local variables
+    var args = process.argv.slice(2);//receives arguments from command line
+	
+	//Debug
+	console.log(args);
+    
+	var result = false;
     var a=0;
     var block = 1000;
-    getDomains();
-    //Main loop only stops if result is true
+	
+	//Main loop only stops if result is true
     while (result !== true){
         //push into the queue the object
         if (postQueue.getLength() <= maxQueueSize ){
-            arrayPosts[a] = new postJob(arguments,domains[a],block);
+            arrayPosts[a] = new postJob(config.locations[a], block);
             postQueue.enqueue(arrayPosts[a]);
         }
         //pulls the object from the queue if returns false
