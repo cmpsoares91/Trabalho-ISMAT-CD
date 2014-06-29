2a8ebb5152475fd00ca8bc0b50d34f5fcd001433
Nuno Miguel de Carvalho Ferreira <nuno.carvalhoferreira@outlook.com>
2014-06-29 13:23:26 +0100

    new functions
    Nova função getDomain, alteração de var postJob para objecto, introdução
    da queue, função main.

--- Cliente/client.js
+++ Cliente/client.js
@@ -6,16 +6,25 @@
  
  /**
  *  An example HTTP POST request.
- */  
-var postJob = function(URL, blockNum){
+ */
+//Global variables
+var domains = new Array(); //receive domains from config file;
+var postQueue = new Queue(); //Queue with dor the posts
+var maxQueueSize; //Maximum size of queue will depend of the number of hosts
+var arrayPosts = new Array(); //array de objectos postJob
 
+//postJob object
+function postJob(baseString, URL, blockNum){
+    
+    this.result;
+    
 	var requestObject = {
 		blockSize: bs || 1000;
 		block: blockNum,
-		baseStr: str || "Hello, World!",
+		baseStr: str || baseString,
 		objective: objStr || "00000"
 	};
-
+        var tempresult;
 	$.ajax({
 		'url': URL,
 		'type' : 'POST',
@@ -23,6 +32,7 @@ var postJob = function(URL, blockNum){
 		'data' : JSON.stringify(requestObject), 
 		'processData' : false,
 		'success' : function(data){
+                        tempresult = jQuery.parseJSON(data)
 			console.log(data);
 		},
 		'error': function(jqXHR, data){
@@ -30,4 +40,50 @@ var postJob = function(URL, blockNum){
 	    },
 		'dataType' : 'json'
 	});
-};
+        this.result = tempresult;
+}
+
+//function to get the domains from config.json
+function getDomains(){
+    var temp;
+    var tempSize;
+    $.getJSON('example.json', function(response){
+       temp = response;
+       tempSize = Object.keys(response).length;
+    });
+    domains = temp;
+    maxQueueSize = tempsize;
+}
+
+//main function
+function main(){
+    //local variables
+    var arguments = process.argv.slice(2);//receives arguments from command line
+    var result = false;
+    var a=0;
+    var block = 1000;
+    getDomains();
+    //Main loop only stops if result is true
+    while (result !== true){
+        //push into the queue the object
+        if (postQueue.getLength() <= maxQueueSize ){
+            arrayPosts[a] = new postJob(arguments,domains[a],block);
+            postQueue.enqueue(arrayPosts[a]);
+        }
+        //pulls the object from the queue if returns false
+        if (arrayPosts[a].result == 'false'){
+            postQueue.dequeue(arrayPosts[a]);
+        } 
+        //if it returns true stops the main loop
+        else if (arrayPosts[a].result == 'true'){
+            result = true;
+        }
+        a++;
+        block = block+1000;
+        if ( a > maxQueueSize){
+            a = 0;
+        }
+        
+    }
+    
+}
