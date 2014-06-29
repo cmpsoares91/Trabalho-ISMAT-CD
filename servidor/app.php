80a99f5dc53d3aba0eb780b82c488b2f73b2a66c
Nuno Miguel de Carvalho Ferreira <nuno.carvalhoferreira@outlook.com>
2014-06-29 14:40:12 +0100

    correcção dos $

--- servidor/app.php
+++ servidor/app.php
@@ -1,10 +1,10 @@
 <?php
 	if (!$_POST['data'] == ""){
 		$data = json_decode($_POST['data']);
-		$string = data->{'baseStr'};
-		$min = data->{'block'} * data->{'blockSize'};
-		$max = $min + data->{'blockSize'} - 1;
-		$result = data->{'objective'};
+		$string = $data->{'baseStr'};
+		$min = $data->{'block'} * $data->{'blockSize'};
+		$max = $min + $data->{'blockSize'} - 1;
+		$result = $data->{'objective'};
 		
 		$size = strlen($result);
 		for ($i=$min; $i<$max; ++$i){
