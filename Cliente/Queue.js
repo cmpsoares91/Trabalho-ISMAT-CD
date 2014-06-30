98d8db2584d224ac787ce8601545fd2650498c25
Carlos Soares <cmpsoares91@gmail.com>
2014-06-30 19:42:03 +0100

    Adding original Queue.js

--- Cliente/Queue.js
+++ Cliente/Queue.js
@@ -1,53 +1,71 @@
 //Original JS function: http://code.stephenmorley.org/javascript/queues/
-Queue: function(){
-	// initialise the queue and offset
-	var queue  = [];
-	var offset = 0;
+/*
 
-	// Returns the length of the queue.
-	this.getLength = function(){
-	return (queue.length - offset);
-	}
+Queue.js
 
-	// Returns true if the queue is empty, and false otherwise.
-	this.isEmpty = function(){
-	return (queue.length == 0);
-	}
+A function to represent a queue
 
-	/* Enqueues the specified item. The parameter is:
-	*
-	* item - the item to enqueue
-	*/
-	this.enqueue = function(item){
-	queue.push(item);
-	}
+Created by Stephen Morley - http://code.stephenmorley.org/ - and released under
+the terms of the CC0 1.0 Universal legal code:
 
-	/* Dequeues an item and returns it. If the queue is empty, the value
-	* 'undefined' is returned.
-	*/
-	this.dequeue = function(){
+http://creativecommons.org/publicdomain/zero/1.0/legalcode
 
-	// if the queue is empty, return immediately
-	if (queue.length == 0) return undefined;
+*/
 
-	// store the item at the front of the queue
-	var item = queue[offset];
+/* Creates a new queue. A queue is a first-in-first-out (FIFO) data structure -
+ * items are added to the end of the queue and removed from the front.
+ */
+function Queue(){
 
-	// increment the offset and remove the free space if necessary
-	if (++ offset * 2 >= queue.length){
-	  queue  = queue.slice(offset);
-	  offset = 0;
-	}
+  // initialise the queue and offset
+  var queue  = [];
+  var offset = 0;
 
-	// return the dequeued item
-	return item;
+  // Returns the length of the queue.
+  this.getLength = function(){
+    return (queue.length - offset);
+  }
 
-	}
+  // Returns true if the queue is empty, and false otherwise.
+  this.isEmpty = function(){
+    return (queue.length == 0);
+  }
 
-	/* Returns the item at the front of the queue (without dequeuing it). If the
-	* queue is empty then undefined is returned.
-	*/
-	this.peek = function(){
-	return (queue.length > 0 ? queue[offset] : undefined);
-	}
+  /* Enqueues the specified item. The parameter is:
+   *
+   * item - the item to enqueue
+   */
+  this.enqueue = function(item){
+    queue.push(item);
+  }
+
+  /* Dequeues an item and returns it. If the queue is empty, the value
+   * 'undefined' is returned.
+   */
+  this.dequeue = function(){
+
+    // if the queue is empty, return immediately
+    if (queue.length == 0) return undefined;
+
+    // store the item at the front of the queue
+    var item = queue[offset];
+
+    // increment the offset and remove the free space if necessary
+    if (++ offset * 2 >= queue.length){
+      queue  = queue.slice(offset);
+      offset = 0;
+    }
+
+    // return the dequeued item
+    return item;
+
+  }
+
+  /* Returns the item at the front of the queue (without dequeuing it). If the
+   * queue is empty then undefined is returned.
+   */
+  this.peek = function(){
+    return (queue.length > 0 ? queue[offset] : undefined);
+  }
+
 }
