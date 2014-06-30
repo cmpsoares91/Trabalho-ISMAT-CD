26654d5721555f1a1e6e39846fe783eaf4c73314
Carlos Soares <cmpsoares91@gmail.com>
2014-06-30 19:28:17 +0100

    jquery defined unnodification started

--- Cliente/Queue.js
+++ Cliente/Queue.js
@@ -1,61 +1,53 @@
 //Original JS function: http://code.stephenmorley.org/javascript/queues/
-//Edited to work as a Node.js tool
+Queue: function(){
+	// initialise the queue and offset
+	var queue  = [];
+	var offset = 0;
 
-module.exports = {
-	/* Creates a new queue. A queue is a first-in-first-out (FIFO) data structure -
-	* items are added to the end of the queue and removed from the front.
+	// Returns the length of the queue.
+	this.getLength = function(){
+	return (queue.length - offset);
+	}
+
+	// Returns true if the queue is empty, and false otherwise.
+	this.isEmpty = function(){
+	return (queue.length == 0);
+	}
+
+	/* Enqueues the specified item. The parameter is:
+	*
+	* item - the item to enqueue
 	*/
-	Queue: function(){
-	  // initialise the queue and offset
-	  var queue  = [];
-	  var offset = 0;
+	this.enqueue = function(item){
+	queue.push(item);
+	}
 
-	  // Returns the length of the queue.
-	  this.getLength = function(){
-		return (queue.length - offset);
-	  }
+	/* Dequeues an item and returns it. If the queue is empty, the value
+	* 'undefined' is returned.
+	*/
+	this.dequeue = function(){
 
-	  // Returns true if the queue is empty, and false otherwise.
-	  this.isEmpty = function(){
-		return (queue.length == 0);
-	  }
+	// if the queue is empty, return immediately
+	if (queue.length == 0) return undefined;
 
-	  /* Enqueues the specified item. The parameter is:
-	   *
-	   * item - the item to enqueue
-	   */
-	  this.enqueue = function(item){
-		queue.push(item);
-	  }
+	// store the item at the front of the queue
+	var item = queue[offset];
 
-	  /* Dequeues an item and returns it. If the queue is empty, the value
-	   * 'undefined' is returned.
-	   */
-	  this.dequeue = function(){
+	// increment the offset and remove the free space if necessary
+	if (++ offset * 2 >= queue.length){
+	  queue  = queue.slice(offset);
+	  offset = 0;
+	}
 
-		// if the queue is empty, return immediately
-		if (queue.length == 0) return undefined;
-
-		// store the item at the front of the queue
-		var item = queue[offset];
-
-		// increment the offset and remove the free space if necessary
-		if (++ offset * 2 >= queue.length){
-		  queue  = queue.slice(offset);
-		  offset = 0;
-		}
-
-		// return the dequeued item
-		return item;
-
-	  }
-
-	  /* Returns the item at the front of the queue (without dequeuing it). If the
-	   * queue is empty then undefined is returned.
-	   */
-	  this.peek = function(){
-		return (queue.length > 0 ? queue[offset] : undefined);
-	  }
+	// return the dequeued item
+	return item;
 
 	}
-};
+
+	/* Returns the item at the front of the queue (without dequeuing it). If the
+	* queue is empty then undefined is returned.
+	*/
+	this.peek = function(){
+	return (queue.length > 0 ? queue[offset] : undefined);
+	}
+}
--- Cliente/client.js
+++ Cliente/client.js
@@ -1,26 +1,13 @@
-/**
- * Client application in Node.js
- */
-var http = require('http');
-var querystring = require('querystring');
 var config = require('./config.json'); //config.locations.length e config.locations[0] devem estar a funcionar agora...
 var Q      = require('./Queue.js');
- 
- //Debug
- console.log(typeof Q.Queue);
- console.log(config.locations.length);
- console.log(config.locations[0]);
- console.log(config.blockSize);
- 
- /**
- *  An example HTTP POST request.
- */
+
 //Global variables
 var blockQueue   = new Q.Queue(); //Queue with the posts
 var arrayPosts   = new Array();   //postJob Object array
+var result       = false; 		  //flag for cycle
 
 //Load configuration data to variables
-var maxPosts = config.locations.length; //Maximum number of posts working simultaneously, it depends on the number of hosts
+var maxPosts     = config.locations.length; //Maximum number of posts working simultaneously, it depends on the number of hosts
 var bSize, objString, bString;
 //bSize load:
 try {
@@ -56,142 +43,62 @@ main();
 //postJob object
 function postJob(URL, blockNum){
     
-	//debug
-	console.log("postjob called");
-
 	//Define Post request object
-	var requestObject       = {
-	blockSize : bSize || 1000,
-	base      : bString || "Hello, World!",
-	objective : objString || "00000",
-	block     : blockNum
-        };
-	var url = 'http://' + URL + '/app.php';
-	//var codeString = JSON.stringify(requestObject);
-	//var result;
-	
-	//debug
-	console.log("body: " + JSON.stringify(requestObject));
+	var requestObject       = {};
+	requestObject.blockSize = bSize || 1000;
+	requestObject.base      = bString || "Hello, World!";
+	requestObject.objective = objString || "00000";
+	requestObject.block     = blockNum;
 
-	// Build the post string from an object
-	var post_data = querystring.stringify(requestObject);
-        var postlength = post_data.length;
-        console.log(post_data);
-        
-	// An object of options to indicate where to post to
-	var post_options = {
-	  host: url,
-          port: 80,
-	  path: '/app.php',
-	  method: 'POST',
-          headers: {
-            'Content-Type' : 'application/json',
-            'Content-Length' : postlength
-            }
-	};
-	/*
-	callback = function (res) {
-		//res.setEncoding('utf8');
-		console.log(res);
-		res.on('data', function (chunk) {
-		  str += chunk;
-		});
-		
-		res.on('end', function () {
-		console.log('Response: ' + str);
-		});
-	}*/
+    var result;
 
-	// Set up the request
-        var str = "";
-	var post_req = http.request(post_options, function (res){
-            res.on('data', function (chunk) {
-                console.log(str);
-                str += chunk;
-            });
-            res.on('end', function() { 
-                console.log(str);
-            });
-        });
-	  // post the data
-	post_req.write(post_data);
-	post_req.end();
-	
-	//temporary
-	/*
-	var options = {
-	  url: url,
-	  method: 'POST',
-	  body: "requestObject"
-	};
-
-	var r = request(options, function (error, response, body) {
-		console.log("erro: " + error);
-		console.log(response);
-		console.log(body);
-		if (!error && response.statusCode == 200) {
-			console.log("Response is: " + response);
-			console.log("Error is: " + error);
-			
-			result = JSON.parse(body);
-			console.log(result);
-			if(result.found){
-				console.log("Problem Solved!");
-				console.log("The resolution is: " + result.resolution);
-				console.log("The hash result is: " + result.hash);
-				process.exit(0);
+	$.ajax({
+		type : 'POST',
+		url: URL + '/app.php',
+		data : {"blockSize": requestObject.blockSize, "base": requestObject.base, "objective": requestObject.objective, "block": requestObject.block}, 
+		success : 	function(data, textStatus, jqXHR) {
+                        result = jQuery.parseJSON(data)
+						console.log(tempresult);
+						if(result.found){
+							//Solution found, print that, stop cicle.
+							process.exit(0);
+						}
+						console.log("Not found clearing slave...");
+						arrayPosts[arrayPosts.indexOf(blockNum)] = null;
+					},
+		error: function(jqXHR, textStatus, errorThrown){
+			console.log(textStatus);
+			if(textStatus === 'parsererror') {
+				//parse error occured, print that...
 			}
-			console.log("Not found clearing slave...");
-			arrayPosts[arrayPosts.indexOf(blockNum)] = null;
-		}
-		else {
-			console.log("Error: " + error);
-			console.log("Adding block number to queue...");
-			arrayPosts[arrayPosts.indexOf(blockNum)] = null;
-			blockQueue.enqueue(blockNum);
-		}
+			else {
+				//other error occurred print that and add block number to queue...
+				arrayPosts[arrayPosts.indexOf(blockNum)] = null;
+				blockQueue.enqueue(blockNum);
+			}
+	    }
 	});
-	*/
-
-	console.log("Post sent...");
-	//console.log(r);
 }
+
 
 //main function
 function main(){
     //Debug
 	console.log("main init");
 	
-	//local variables
-    var args = process.argv.slice(2);//receives arguments from command line
-	
-	//Debug
-	console.log(args);
-    
-	var result = false;
+	//Retrieve arguments
+    var bString = document.getElementById("frase");
     var a = 0, block = 0;
 	
 	//Main loop only stops if result is true
     while (result !== true){
 		for(a = 0; a < maxPosts; a++){
-			//debug
-			//console.log("Array spot is: " + arrayPosts[a]);
-			//console.log("It's on block nr: " + block);
 			if(arrayPosts[a] == undefined || arrayPosts[a] == null){
-				console.log("Array spot is undefined: " + arrayPosts[a]);
-				if (blockQueue.isEmpty()) {
-					//debug
-					console.log("Queue is empty");
-					console.log("Link: " + config.locations[a]);
-					console.log("Block: " + block);
-				
+				if (blockQueue.isEmpty()) {				
 					new postJob(config.locations[a], block);
 					arrayPosts[a] = block++;
 				}
 				else {
-					//debug
-					console.log("Queue is not empty");
-					
 					new postJob(config.locations[a], blockQueue.peek());
 					arrayPosts[a] = blockQueue.dequeue();
 				}
